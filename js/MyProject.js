2d0924d4ea7d52ba50e60561a6af65f9e9a14260
Denys Khozieiev <[denishozv@gmail.com]>
2019-11-17 15:10:15 +0200

    Failes for Project

---
+++ js/MyProject.js
@@ -0,0 +1,5 @@
+document.getElementById("footer__item__subscribe__text1").onclick = function (){myFunction()};
+
+function myFunction() {
+    document.getElementById("footer__item__subscribe__text1").innerHTML = "You have been sent a password in the mail";
+}
